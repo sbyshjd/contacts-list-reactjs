c10c34920d0708e2cdddda87f12522001d688e25
Boyuan Shi <sbyshjd@hotmail.com>
2020-09-18 23:21:41 +0200

    adding more unit testings for app

--- src/App.test.js
+++ src/App.test.js
@@ -3,9 +3,9 @@ import ReactDOM from 'react-dom';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders the correct content', () => {
   //test the <App/> has a <h1> tag with 'Contacts List'
-  const root = document.createElement('div')
-  ReactDOM.render(<App />, root)
-  expect(root.querySelector('h1').textContent).toBe('Contacts List')
+  const { getByText } = render(<App/>)
+
+  getByText('Contacts List')
 });
