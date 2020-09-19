d21a0298500847b09c8e9c5754559d5af199582f
Boyuan Shi <sbyshjd@hotmail.com>
2020-09-19 19:04:50 +0200

    change style from css to sass

--- src/App.js
+++ src/App.js
@@ -1,73 +1,91 @@
-import React, { useState, useEffect} from 'react'
-import './App.css'
-import { alphabet } from './basicVariables'
-import contactsListAPI from './components/service/ContactInfo'
-import UserCard from './components/UserCard'
+import React, { useState, useEffect } from "react";
+import "./App.scss";
+import { alphabet } from "./basicVariables";
+import contactsListAPI from "./components/service/ContactInfo";
+import UserCard from "./components/UserCard";
 
-
-
-export default function App () {
-
-  const [contactsList, setContactsList] = useState([])
-  const [contactNumberList, setContactNumberList] = useState([])
-  const [selected, setSelected] = useState(0)
-  const [userSelected, setUserSelected] = useState('')
-
+export default function App() {
+  const [contactsList, setContactsList] = useState([]);
+  const [contactNumberList, setContactNumberList] = useState([]);
+  const [selected, setSelected] = useState(0);
+  const [userSelected, setUserSelected] = useState("");
 
   const closeClick = () => {
-    setUserSelected('')
-  }
+    setUserSelected("");
+  };
 
   useEffect(() => {
-    contactsListAPI()
-    .then(res => {
-      const newContactsList = res.results
-      const newContactNumberList = alphabet.map(letter => newContactsList.filter(p => p.name.last[0].toLowerCase()===letter).length)
-      setContactsList(newContactsList)
-      setContactNumberList(newContactNumberList)
-    })
-  }, [])
+    contactsListAPI().then((res) => {
+      const newContactsList = res.results;
+      const newContactNumberList = alphabet.map(
+        (letter) =>
+          newContactsList.filter((p) => p.name.last[0].toLowerCase() === letter)
+            .length
+      );
+      setContactsList(newContactsList);
+      setContactNumberList(newContactNumberList);
+    });
+  }, []);
 
-    window.addEventListener('click',(e)=> {
-      console.log('............',e.target.tagName)
-      if(e.target.tagName==='HTML') {
-        this.closeClick(e)
-      }
-      
-    })
+  window.addEventListener("click", (e) => {
+    console.log("............", e.target.tagName);
+    if (e.target.tagName === "HTML") {
+      this.closeClick(e);
+    }
+  });
 
-    return (
-      <div className="App" onClick={(e) => { closeClick(e)}}>
-        <div className='container-fluid'>
-          <h1 className='text-center mb-3' style={{fontSize:'2rem'}}>Contacts List</h1>
-        {/* the tab nav part */}
-          <div className='d-flex flex-wrap pt-1'>
-            {alphabet.map((letter,i) => (<button 
-                  className={selected===i ? "flex-fill letter-btn active" : "flex-fill letter-btn"} 
-                  disabled={contactNumberList[i]===0} 
-                  key={letter}
-                  onClick={(e) => setSelected(i)}
-                >
-                {letter} <i className='d-none d-md-block'>{contactNumberList[i]}</i>
-                </button>))}
-          </div>
-        {/* the contacts-list part */}
-          <div className='d-flex flex-wrap justify-content-between pl-3 pr-3 pt-3 pb-5 contacts-list'>
-          {contactsList
-              .filter(p => p.name.last[0].toLowerCase()===alphabet[selected])
-              .map(p => <div className='pt-3 pb-3 contact'
-                        style={{position:'relative'}} 
-                        key={p.login.uuid} 
-                        onClick={(e)=> {
-                          e.stopPropagation()
-                          setUserSelected(p.login.uuid)
-                          }}>
-                        <div className='contact-name'>{p.name.first},  {p.name.last.toUpperCase()}</div>
-          {/* show the detailed page or not */}
-                        {p.login.uuid===userSelected ? <UserCard user={p} closeWindow={closeClick}/> : null}
-                        </div>)}
-          </div>
-        </div>  
+  return (
+    <div
+      className="App"
+      onClick={(e) => {
+        closeClick(e);
+      }}
+    >
+      <h1 className="text-center mb-3" style={{ fontSize: "2rem" }}>
+        Contacts List
+      </h1>
+      {/* the tab nav part */}
+      <div className="nav d-flex flex-wrap pt-1">
+        {alphabet.map((letter, i) => (
+          <button
+            className={
+              selected === i
+                ? "nav__btn nav__btn--active flex-fill"
+                : "nav__btn flex-fill"
+            }
+            disabled={contactNumberList[i] === 0}
+            key={letter}
+            onClick={(e) => setSelected(i)}
+          >
+            {letter}{" "}
+            <i className="nav__num d-none d-md-block">{contactNumberList[i]}</i>
+          </button>
+        ))}
       </div>
-    )
+      {/* the contacts-list part */}
+      <div className="contacts-list d-flex flex-wrap justify-content-between pl-3 pr-3 pt-3 pb-5">
+        {contactsList
+          .filter((p) => p.name.last[0].toLowerCase() === alphabet[selected])
+          .map((p) => (
+            <div
+              className="contacts-list__item pt-3 pb-3"
+              style={{ position: "relative" }}
+              key={p.login.uuid}
+              onClick={(e) => {
+                e.stopPropagation();
+                setUserSelected(p.login.uuid);
+              }}
+            >
+              <div className="contacts-list__name">
+                {p.name.first}, {p.name.last.toUpperCase()}
+              </div>
+              {/* show the detailed page or not */}
+              {p.login.uuid === userSelected ? (
+                <UserCard user={p} closeWindow={closeClick} />
+              ) : null}
+            </div>
+          ))}
+      </div>
+    </div>
+  );
 }
--- src/components/UserCard.js
+++ src/components/UserCard.js
@@ -1,44 +1,65 @@
-import React from 'react'
-import './UserCard.css'
-import { VscChromeClose } from "react-icons/vsc"
-
+import React from "react";
+import "./UserCard.scss";
+import { VscChromeClose } from "react-icons/vsc";
 
 export default function UserCard(props) {
-    return (
-        <div className='user-card d-md-flex align-items-start' style={{position:'absolute',zIndex:10}}
-            onClick={(e)=> {}}
-            >
-                <div className='user-card-img'>
-                    <button className='d-block'
-                    onClick={(e) => { 
-                        e.stopPropagation()
-                        props.closeWindow(e)}}>
-                    <VscChromeClose/>
-                    </button>
-                    <img src={props.user.picture.large} alt='profile'></img>
-                </div>   
-                <div className='user-card-info'> 
-                    <h2>{props.user.name.last.toUpperCase()}, {props.user.name.first.toLowerCase()}</h2>
-                    <div className='d-flex'>
-                        <div className='mr-3'>
-                        <p><strong>e mail</strong></p> 
-                        <p><strong>phone</strong></p> 
-                        <p><strong>street</strong></p> 
-                        <p><strong>state</strong></p> 
-                        <p><strong>postcode</strong></p> 
-                        </div>
-                        <div>
-                        <p>{props.user.email}</p> 
-                        <p>{props.user.phone}</p> 
-                        <p>{props.user.location.street.name} {props.user.location.street.number}</p> 
-                        <p>{props.user.location.state}</p> 
-                        <p>{props.user.location.postcode}</p> 
-                        </div>
-                    </div>     
-                </div>
-            <p className='vertical-rl' style={{position:'absolute',top:'-10px',right:'0px'}}><strong>USRNAME</strong> {props.user.login.username}</p>
-                
-            </div>
-    )
+  return (
+    <div
+      className="user-card d-md-flex align-items-start"
+      style={{ position: "absolute", zIndex: 10 }}
+      onClick={(e) => {}}
+    >
+      <button
+        className="user-card__button d-block"
+        onClick={(e) => {
+          e.stopPropagation();
+          props.closeWindow(e);
+        }}
+      >
+        <VscChromeClose />
+      </button>
+      <img className="user-card__img" src={props.user.picture.large} alt="profile"></img>
+      <div className="user-card__info">
+        <h2 className="user-card__info__name">
+          {props.user.name.last.toUpperCase()},{" "}
+          {props.user.name.first.toLowerCase()}
+        </h2>
+        <div className="d-flex">
+          <div className="mr-3">
+            <p className="user-card__info__detail">
+              <strong>e mail</strong>
+            </p>
+            <p className="user-card__info__detail">
+              <strong>phone</strong>
+            </p>
+            <p className="user-card__info__detail">
+              <strong>street</strong>
+            </p>
+            <p className="user-card__info__detail">
+              <strong>state</strong>
+            </p>
+            <p className="user-card__info__detail">
+              <strong>postcode</strong>
+            </p>
+          </div>
+          <div>
+            <p className="user-card__info__detail">{props.user.email}</p>
+            <p className="user-card__info__detail">{props.user.phone}</p>
+            <p className="user-card__info__detail">
+              {props.user.location.street.name}{" "}
+              {props.user.location.street.number}
+            </p>
+            <p className="user-card__info__detail">{props.user.location.state}</p>
+            <p className="user-card__info__detail">{props.user.location.postcode}</p>
+          </div>
+        </div>
+      </div>
+      <p
+        className="user-card__vertical-rl"
+        style={{ position: "absolute", top: "-10px", right: "0px" }}
+      >
+        <strong>USRNAME</strong> {props.user.login.username}
+      </p>
+    </div>
+  );
 }
-
